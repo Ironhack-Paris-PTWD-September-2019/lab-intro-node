9bc2ea06cd1c616d501e358f6d5d121d70bdc2e7
Sofy-Prd <sofy@DESKTOP-4LRCARK.localdomain>
2019-11-20 15:09:41 +0100

    final commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,54 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
-  };
+    constructor(items, length) {
+      this.items=[];
+      this.length=0;
+    }
+
+    add(item) {
+      this.items.push(item);
+      this.items.sort(function(a,b) {
+        return a-b;
+      })
+      this.length++;
+    }
+
+    get(pos) {
+      if (pos<=this.length){
+        return this.items[pos];
+      } else {
+        throw new Error("OutOfBounds");
+      }
+    }
+
+    max() {
+      if (this.items.length===0) {
+        throw new Error("EmptySortedList");
+      }
+      return Math.max(...this.items);
+    }
+
+    min() {
+      if (this.items.length===0) {
+        throw new Error("EmptySortedList");
+      }
+      return Math.min(...this.items);
+    }
+    
+    average() {
+      if (this.items.length===0) {
+        throw new Error("EmptySortedList");
+      }
+      return this.sum()/this.items.length
+      }
+
+    sum() {
+      if (this.items.length===0) {
+        return 0;
+      } else {
+      return this.items.reduce((accumulator, currentValue) => accumulator + currentValue)
+        }
+    }
+
+  }
   
   module.exports = SortedList;
