c90ad62de3c5da8281afd4d39d839f89296c320b
Sandrine8304 <sandrine.auberval@gmail.com>
2019-11-20 22:32:22 +0100

    lab completed

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,55 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor(items, length) {
+      this.items = [];
+      this.length = 0;
+    }
+    add(item) {
+      this.items.push(item);
+      this.items.sort(function(a,b){
+        return a-b;
+      })
+      this.length += 1;
+    }
+    get(pos) {
+      if (pos <= this.length) {
+        return this.items[pos];
+      } else {
+        throw new Error("OutOfBounds");
+      }
+    }
+    max() {
+      if (this.length === 0) {
+        throw new Error("EmptySortedList");
+      } else {
+        return Math.max(...this.items);
+      }      
+    }
+    min() {
+      if (this.length === 0) {
+        throw new Error("EmptySortedList")
+      } else {
+        return Math.min(...this.items);
+      } 
+    }
+    sum() {
+      let somme = 0;
+      if (this.length === 0) {
+        return 0;
+      } else {
+        for (let i=0 ; i<this.length ; i++) {
+          somme = somme + this.items[i];
+        }
+        return somme;
+      }      
+    }
+    average() {
+      if (this.length === 0) {
+        throw new Error("EmptySortedList");
+      } else {
+        return this.sum()/this.length;
+      }      
+    }
+    
   };
   
   module.exports = SortedList;
